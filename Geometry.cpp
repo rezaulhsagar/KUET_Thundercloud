356cd32c84cbf4cc97840dc9abc9a35fb38f67de
Rezaul Hoque Sagar <rezaulhsagar@gmail.com>
2018-08-10 20:18:17 +0600

    Update Geometry.cpp

--- Geometry.cpp
+++ Geometry.cpp
@@ -1,8 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-const int MAXD = 2;
 const double pi = acos (-1.0);
 const double eps = 1e-8;
 
@@ -12,50 +7,68 @@ int dblcmp (double d){
 }
 
 struct Vector {
-    int d;
-    double val[MAXD];
+    double x, y;
 
-    Vector (){d = 2;}
-
-
-    Vector operator + (Vector b){
-        Vector res;
-        for (int i = 0; i < d; i++) res.val[i] = val[i] + b.val[i];
-        return res;
+    Vector (){}
+    Vector (double a, double b){
+        x = a; y = b;
     }
 
-    Vector operator - (Vector b){
-        Vector res;
-        for (int i = 0; i < d; i++) res.val[i] = val[i] - b.val[i];
-        return res;
+    Vector operator + (Vector a){
+        return Vector (x + a.x, y + a.y);
+    }
+
+    Vector operator - (Vector a){
+        return Vector (x - a.x, y - a.y);
     }
 
     Vector operator * (double t){
-        Vector res;
-        for (int i = 0; i < d; i++) res.val[i] = val[i] * t;
-        return res;
+        return Vector (x * t, y * t);
     }
 
     Vector operator / (double t){
-        Vector res;
-        for (int i = 0; i < d; i++) res.val[i] = val[i] / t;
-        return res;
+        return Vector (x / t, y / t);
     }
 
-    bool operator == (Vector b){
-        for (int i = 0; i < d; i++)
-            if (!dblcmp (val[i] - b.val[i])) return false;
-        return true;
+    bool operator == (Vector a){
+        return (dblcmp (a.x - x) == 0 and dblcmp (a.y - y) == 0);
     }
 
-    double dot (Vector v){
-        double res = 0;
-        for (int i = 0; i < d; i++) res += (val[i] * v.val[i]);
-        return res;
+    double dot (Vector a){
+        return (x * a.x + y * a.y);
     }
 
     double dist (){
         return sqrt (dot (*this));
     }
-
 };
+
+double triArea (Vector a, Vector b, Vector c){
+    return (a.cross (b) + b.cross (c) - c.cross (a)) / 2.0;
+}
+
+double polygonArea (vector <Vector>& P){
+    double ret = 0.0;
+    for (int i = 0; i < (int) P.size() - 1; i++){
+        ret += P[i].cross (P[i + 1]);
+    }
+    return ret / 2.0;
+}
+
+double circleCircleArea (Vector a, double r1, Vector b, double r2){  //not sure of this function, strong dataset like codeforces gives WA
+    double c = (a - b).dist();
+    if (c >= (r1 + r2)) return 0.0; //fully outside
+    if (c <= fabs (r1 - r2)){
+        if (r2 < r1) return pi * r2 * r2;
+        return pi * r1 * r1; //fully inside
+    }
+    //partially overlapped
+    double ret = 0.0;
+    double t1 = 2.0 * (acos ((r1 * r1 + c * c - r2 * r2) / (2.0 * r1 * c)));
+    double t2 = 2.0 * (acos ((r2 * r2 + c * c - r1 * r1) / (2.0 * r2 * c)));
+    ret += (0.5 * t1 * r1 * r1);
+    ret += (0.5 * t2 * r2 * r2);
+    ret -= (0.5 * r1 * r1 * sin (t1));
+    ret -= (0.5 * r2 * r2 * sin (t2));
+    return ret;
+}
